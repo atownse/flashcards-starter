d97215d44b2c56ccba71a70a10ef7fcf60885eca
Aaron Townsend <atownse17@gmail.com>
2020-10-08 17:55:59 -0500

    Complete functionality of game class and add assets

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,26 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('./Card');
+const Deck = require('./deck');
+const Round = require('./round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null;
+  }
 
-  printMessage(deck, round) {
+  start() {
+    const cards = prototypeQuestions.map(card => {
+      return new Card(card.id, card.question, card.answers, card.correctAnswer)
+    });
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck);
+    this.printQuestion(this.currentRound);
+  }
+
+  printMessage(deck) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
